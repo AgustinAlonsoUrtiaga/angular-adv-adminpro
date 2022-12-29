dfc06344533f0269001f988c9f3237dba862e2fa
Agustin Alonso Urtiaga <aurtiaga@smsargentina.com.ar>
2022-12-29 13:57:08 -0300

    router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,23 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
-import { DashboardComponent } from './page/dashboard/dashboard.component';
-import { Grafica1Component } from './page/grafica1/grafica1.component';
-import { NoPageFoundComponent } from './page/no-page-found/no-page-found.component';
-import { PagesComponent } from './page/pages.component';
-import { ProgressComponent } from './page/progress/progress.component';
+import { NoPageFoundComponent } from './no-page-found/no-page-found.component';
 
 const routes: Routes = [
-  { 
-    path: '' ,
-    component: PagesComponent,
-    children: [
-      { path: 'dashboard' ,component: DashboardComponent },
-      { path: 'progress',component: ProgressComponent},
-      { path: 'grafica1', component: Grafica1Component},
-      { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-    ]},
-
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
   
