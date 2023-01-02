26e6c24195f68b29f4d0aef113e47af6e260a657
Agustin Alonso Urtiaga <aurtiaga@smsargentina.com.ar>
2023-01-02 11:17:49 -0300

    sidebar service

--- src/app/page/pages.component.ts
+++ src/app/page/pages.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { SettingsService } from '../services/settings.service';
+declare function customInitFunction():any;
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
   styles: [
   ]
 })
-export class PagesComponent {
+export class PagesComponent implements OnInit {
+  public linkTheme = document.querySelector('#theme');
+
+  constructor(private settingService: SettingsService){
+
+  }
+
+  ngOnInit(): void {
+    const url = localStorage.getItem('theme') || `./assets/css/colors/purple-dark.css`;
+
+    this.linkTheme?.setAttribute('href',url);
+    customInitFunction();
+  }
 
 }
